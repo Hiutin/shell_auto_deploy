be938f9f2eed11d66b2d864d2e5086aede175a14
hiutin <xiaotian.hongkong@gmail.com>
2019-05-21 01:04:43 +0800

    update shell

--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # /Users/aaronyu/Dropbox/vsystems/mainchain/v-systems/target
-read -p "Enter the address of a node to be connected: "  node_address
+read -p "Enter the address of a node to be connected: " node_address
 echo "The node address is $node_address"
 read -p "Enter the path of the target file: " target_file_path
-target_file="$target_file_path/vsys_all_*.zip"
-echo $target_file
-# echo "The input target file is: $target_file_path"
-# cp target/vsys-all-*.jar ../vsys-node/v-systems.jar
+target_file="$target_file_path/vsys-all-*.jar"
+if [ -f $target_file ]; then
+  echo "Get the target file: " $target_file
+else
+  echo "Error: The target file does not exsit!"
+  exit 1
+fi
+echo "go go go"
