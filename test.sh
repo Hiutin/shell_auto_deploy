58ba39da55376658ef86bb3490578004d100c15e
hiutin <xiaotian.hongkong@gmail.com>
2019-05-20 22:45:06 +0800

    update the bash file

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-name='nam'
-echo 'hello';
-echo 'this';
-echo 'that';
+
+# /Users/aaronyu/Dropbox/vsystems/mainchain/v-systems/target
+read -p "Enter the address of a node to be connected: "  node_address
+echo "The node address is $node_address"
+read -p "Enter the path of the target file: " target_file_path
+target_file="$target_file_path/vsys_all_*.zip"
+echo $target_file
+# echo "The input target file is: $target_file_path"
+# cp target/vsys-all-*.jar ../vsys-node/v-systems.jar
